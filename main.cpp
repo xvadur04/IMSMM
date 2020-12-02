8fcc51d83cb872ceeaeef0666630ab42b8fcf074
xvadur04 <xvadur04@vutbr.cz>
2020-12-02 14:30:43 +0100

    načtení dat

--- main.cpp
+++ main.cpp
@@ -10,10 +10,47 @@
 #include <iostream>
 #include "simlib.h"
 #include <dirent.h>
+#include <ctime>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <map>
+#include <bits/stdc++.h>
 
 using std::cout; using std::cin;
 using std::endl; using std::vector;
+
+
+
+class data {
+    public:
+        std::string Date;
+        double Open; //1
+        double High; //2
+        double Low; //3
+        double Close; //4
+        double VolumeUSD; //5
+        double MarketCapUSD; //6
+        data(std::string date, double open, double high, double low, double close, double volumeUSD, double marketCapUSD);
+
+};
+
+data::data(std::string date, double open, double high, double low, double close, double volumeUSD, double marketCapUSD)
+{
+    Date = date;
+    Open = open;
+    High = high;
+    Low = low;
+    Close = close;
+    VolumeUSD = volumeUSD;
+    MarketCapUSD = marketCapUSD;
+}
+
 std::vector<std::vector<std::pair<std::string, std::vector<int>>>> entryData;
+
+std::map<std::string, std::vector<data>> LoadedData;
+
 
 std::vector<std::pair<std::string, std::vector<int>>> read_csv(std::string filename){
     // Reads a CSV file into a vector of <string, vector<int>> pairs where
@@ -30,48 +67,67 @@ std::vector<std::pair<std::string, std::vector<int>>> read_csv(std::string filen
 
     // Helper vars
     std::string line, colname;
-    int val;
 
-    // Read the column names
-    if(myFile.good())
-    {
-        // Extract the first line in the file
-        std::getline(myFile, line);
+    // // Read the column names
+    // if(myFile.good())
+    // {
+    //     // Extract the first line in the file
+    //     std::getline(myFile, line);
 
-        // Create a stringstream from line
-        std::stringstream ss(line);
+    //     // Create a stringstream from line
+    //     std::stringstream ss(line);
 
-        // Extract each column name
-        while(std::getline(ss, colname, ',')){
+    //     // Extract each column name
+    //     while(std::getline(ss, colname, ',')){
 
-            // Initialize and add <colname, int vector> pairs to result
-            result.push_back({colname, std::vector<int> {}});
-        }
-    }
+    //         // Initialize and add <colname, int vector> pairs to result
+    //         result.push_back({colname, std::vector<int> {}});
+    //     }
+    // }
 
+    std::getline(myFile, line); //First row
     // Read data, line by line
+    std::string segment;
+    std::vector<std::string> seglist;
+    std::vector<data> all;
     while(std::getline(myFile, line))
     {
-        // Create a stringstream of the current line
         std::stringstream ss(line);
-
-        // Keep track of the current column index
-        int colIdx = 0;
-
-        // Extract each integer
-        while(ss >> val){
-
-            // Add the current integer to the 'colIdx' column's values vector
-            result.at(colIdx).second.push_back(val);
-
-            // If the next token is a comma, ignore it and move on
-            if(ss.peek() == ',') ss.ignore();
-
-            // Increment the column index
-            colIdx++;
+        std::string item;
+        std::vector<std::string> splittedStrings;
+        while (std::getline(ss, item, ','))
+        {
+            splittedStrings.push_back(item);
         }
-    }
 
+        
+
+        data datas(splittedStrings[0], std::stod(splittedStrings[1]),std::stod(splittedStrings[2]),
+                    std::stod(splittedStrings[3]),std::stod(splittedStrings[4]),
+                    std::stod(splittedStrings[5]),std::stod(splittedStrings[6]));
+
+        all.push_back(datas);
+        
+            
+
+        // // Keep track of the current column index
+        
+
+        // // Extract each integer
+        // while(ss >> val){
+
+        //     // Add the current integer to the 'colIdx' column's values vector
+        //     result.at(colIdx).second.push_back(val);
+
+        //     // If the next token is a comma, ignore it and move on
+        //     if(ss.peek() == ',') ss.ignore();
+
+        //     // Increment the column index
+        //     colIdx++;
+        // }
+    }
+    std::string name = filename.substr(6,3);
+    LoadedData.insert(std::pair<std::string, std::vector<data>>(name,all));
     // Close file
     myFile.close();
 
@@ -94,7 +150,7 @@ void LoadData()
 
     for(std::string file: files)
     {
-        if(file != "." || file != "..")
+        if(file != "." && file != "..")
         {
             entryData.push_back(read_csv("./day/" + file));
         }
